57ed1035100c623cc5220492bb1300cdf445fb83
Tim Zubov <timofey.zubov.05@gmail.com>
2024-05-14 21:46:06 +0300

    Add logger and simple network scanner class. MERGE

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import yaml
+import logging
 
-with open("config.yml") as f:
-    cfg = yaml.load(f, Loader=yaml.FullLoader)
-    print(cfg)
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("../logs/log_file.log"),
+            logging.StreamHandler()
+        ]
+    )
+    with open("config.yml") as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+        print(cfg)
+
