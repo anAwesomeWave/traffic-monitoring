21e8f4cca9b5731451ffe7734e6c7ca2f92a447d
Tim Zubov <timofey.zubov.05@gmail.com>
2024-05-18 14:33:34 +0300

    for import

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import yaml
 import logging
 from logging.config import fileConfig
+import argparse
 
+import yaml
 
 from scan_network import scanner
 from scan_pcap import pcap_scanner
 
+NMAP_HELP = """
+SCAN NETWORK THAT YOUR ENTERED
+Usage:
+ network_scan 192.168.1.71/24
+
+Options:
+ -fs --full_scan 
+
+"""
 fileConfig(
     'logger_config.conf',
     disable_existing_loggers=False,
@@ -14,20 +24,59 @@ fileConfig(
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    # logging.basicConfig(
-    #     level=logging.INFO,
-    #     format="%(asctime)s [%(levelname)s] %(message)s",
-    #     handlers=[
-    #         logging.FileHandler("../logs/log_file.log"),
-    #         logging.StreamHandler()
-    #     ]
-    # )
-    logger.info(123213213)
+    # # logging.basicConfig(
+    # #     level=logging.INFO,
+    # #     format="%(asctime)s [%(levelname)s] %(message)s",
+    # #     handlers=[
+    # #         logging.FileHandler("../logs/log_file.log"),
+    # #         logging.StreamHandler()
+    # #     ]
+    # # )
+    # logger.info(123213213)
+    #
+    # my_scanner = scanner.Scanner()
+    # my_scanner.scan_network()
+    #
+    # with open("config.yml") as f:
+    #     cfg = yaml.load(f, Loader=yaml.FullLoader)
+    #     print(cfg)
+    #
+    parser = argparse.ArgumentParser(
+        prog='Network monitoring',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    my_scanner = scanner.Scanner()
-    my_scanner.scan_network()
+    subparsers = parser.add_subparsers(
+        title='subcommands',
+        help='sub-command help',
+    )
 
-    with open("config.yml") as f:
-        cfg = yaml.load(f, Loader=yaml.FullLoader)
-        print(cfg)
+    parser_nmap = subparsers.add_parser(
+        'network_scan',
+        help='scan give network. Usage: main.py network_scan 192.168.1.0/24',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser_nmap.add_argument('network', type=str, help='bar help')
+    parser_nmap.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        default=True,
+        help='Provide info about collected data to the terminal. Works better with -fs'
+    )
+    parser_nmap.add_argument(
+        '-fs',
+        '--fullscan',
+        action='store_true',
+        default=False,
+        help='fullscan all found hosts in network.'
+    )
+    parser_nmap.add_argument(
+        '-m',
+        '--send_metrics_to_server',
+        action='store_true',
+        default=True,
+        help='Send metrics `total_hosts_discovered`,`total_ports_discovered`.'
+    )
 
+    args = parser.parse_args()
